09a13baafc535e539a8f922fa7335436dcd7364f
Wawira-Muthoni <christinewawiramuthoni@gmail.com>
2022-03-03 23:35:10 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,28 @@
+fun main(){
+    val school = "akirachix"
+    val word =(school[0].toString() + school[2] +school[3])
+    println(word)
+
+val x =statement("Christine",20)
+    println(x)
+
+    fruits()
+
+    sentence()
+}
+fun statement(name:String,age:Int):String{
+    val x = ("Hi my name is $name and I am $age years old")
+    return (x)
+}
+fun fruits(){
+    val fruit= "watermelon "
+    println(fruit.length)
+}
+fun sentence(){
+    val name ="Christine Wawira"
+    if(name == "Christine Wawira")
+        println("That's me!")
+    else("I don't know who that is!")
+}
+
+
